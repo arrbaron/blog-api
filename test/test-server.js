305b75caca8a319ee6a248e9dbdeb0f9921bbeea
Roy Baron <baron.roy@gmail.com>
2017-11-14 17:39:52 -0500

    fix POST test typo

--- test/test-server.js
+++ test/test-server.js
@@ -42,7 +42,7 @@ describe("Blog Posts", function() {
                 res.should.have.status(201);
                 res.should.be.json;
                 res.body.should.be.a("object");
-                res.body.should.include.keys("id", "title", "content", "author", "date");
+                res.body.should.include.keys("id", "title", "content", "author", "publishDate");
                 res.body.id.should.not.be.null;
                 res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
             });
